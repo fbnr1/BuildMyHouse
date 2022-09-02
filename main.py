6b52dbaa0557d12d7e6947b6e1a1b7522a79cb5a
Marlon <nobis222@gmail.com>
2022-09-02 11:12:30 +0200

    demo

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import dearpygui.dearpygui as dpg
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+    with dpg.window(label="Example Window"):
+        dpg.add_text("Hello, world")
+        dpg.add_button(label="Save")
+        dpg.add_input_text(label="string", default_value="Quick brown fox")
+        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
 
