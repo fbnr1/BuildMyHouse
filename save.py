90e76041a98a26b555d136849ce9c211a9952380
lihell <lhellwage@t-h.de>
2022-09-24 19:03:12 +0200

    updated GUI: save commited

---
+++ save.py
@@ -0,0 +1,33 @@
+import codecs
+import json
+import os.path
+
+
+def save(lists, filename):
+    save_path = '.\save'
+    name = os.path.join(save_path, filename)
+    name = name + '.jsonl'
+    newfile = codecs.open(name, "w+", encoding="utf8", errors="ignore")
+    newfile.write(json.dumps(lists))
+
+
+def load(filename):
+    save_path = '.\save'
+    name = os.path.join(save_path, filename)
+    name = name + '.jsonl'
+    newfile = codecs.open(name, "r+", encoding="utf8", errors="ignore")
+    liste = {}
+    for i in newfile:
+        liste = json.loads(i)
+    return liste
+
+class Save(object):
+    def __int__(self, name):
+        self.name = name
+
+
+'''if __name__ == '__main__':
+    list = {'test': "test"}
+    #save(list, "test")
+    liste = load("test")
+    print(liste)'''
