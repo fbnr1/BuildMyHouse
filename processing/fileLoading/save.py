eff5868679cb4d3aab3ab3bbe55a1f52c4cbfba6
Saotex <35031956+Saotex@users.noreply.github.com>
2022-10-04 19:57:36 +0200

    Implemented Window Load

--- processing/fileLoading/save.py
+++ processing/fileLoading/save.py
@@ -7,7 +7,7 @@ from GUI.drawing import draw
 
 def save(lists, filename):
     print(lists)
-    save_path = '../../save'
+    save_path = 'save'
     name = os.path.join(save_path, filename)
     name = name + '.jsonl'
     newfile = codecs.open(name, "w+", encoding="utf8", errors="ignore")
@@ -16,7 +16,7 @@ def save(lists, filename):
 
 def load(filename):
     print("loading...")
-    save_path = '../../save'
+    save_path = 'save'
     name = os.path.join(save_path, filename)
     newfile = codecs.open(name, "r+", encoding="utf8", errors="ignore")
     liste = {}
@@ -24,4 +24,7 @@ def load(filename):
         liste = json.loads(i)
     for floor in liste['House']:
         draw.append_floor(liste['House'][floor])
+        for window in liste['House'][floor]['Windows']:
+            print(liste['House'][floor]['Windows'])
+            draw.draw_window(liste['House'][floor]['Windows'][window])
     return liste
