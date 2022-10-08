c15673d7c16d72bb4d8163efa389d079d7dc15ba
Marlon Nobis <nobis222@gmail.com>
2022-10-08 17:23:07 +0200

    adding roof to nodetree

--- GUI/nodetree.py
+++ GUI/nodetree.py
@@ -16,27 +16,33 @@ def nodes():
                 except:
                     pass
                 current_floor = house_list[house][floor]
-                with dpg.tree_node(label=current_floor["floor_name"], parent=house, tag=floor):
-                    # todo: is deleted?
-                    add_tree_node("Height", current_floor["floor_height"])
-                    add_tree_node("Width", current_floor["floor_width"])
-                    # todo: only create if not empty
-                    with dpg.tree_node(label="Windows"):
-                        for window in current_floor["Windows"]:
-                            current_window = current_floor["Windows"][window]
-                            with dpg.tree_node(label=window):
-                                add_tree_node("Type", current_window["window_type"])
-                                add_tree_node("Height", current_window["window_height"])
-                                add_tree_node("Width", current_window["window_width"])
-                                add_tree_node("Side", current_window["side"])
-                    with dpg.tree_node(label="Doors"):
-                        for door in current_floor["Doors"]:
-                            current_door = current_floor["Doors"][door]
-                            with dpg.tree_node(label=door):
-                                add_tree_node("Type", current_door["door_type"])
-                                add_tree_node("Height", current_door["height"])
-                                add_tree_node("Width", current_door["width"])
-                                add_tree_node("Side", current_door["side"])
+                if floor == "Roof":
+                    with dpg.tree_node(label="Roof", parent=house, tag=floor):
+                        for r in house_list[house][floor]:
+                            with dpg.tree_node(label=r, parent=floor):
+                                pass
+                else:
+                    with dpg.tree_node(label=current_floor["floor_name"], parent=house, tag=floor):
+                        # todo: is deleted?
+                        add_tree_node("Height", current_floor["floor_height"])
+                        add_tree_node("Width", current_floor["floor_width"])
+                        # todo: only create if not empty
+                        with dpg.tree_node(label="Windows"):
+                            for window in current_floor["Windows"]:
+                                current_window = current_floor["Windows"][window]
+                                with dpg.tree_node(label=window):
+                                    add_tree_node("Type", current_window["window_type"])
+                                    add_tree_node("Height", current_window["window_height"])
+                                    add_tree_node("Width", current_window["window_width"])
+                                    add_tree_node("Side", current_window["side"])
+                        with dpg.tree_node(label="Doors"):
+                            for door in current_floor["Doors"]:
+                                current_door = current_floor["Doors"][door]
+                                with dpg.tree_node(label=door):
+                                    add_tree_node("Type", current_door["door_type"])
+                                    add_tree_node("Height", current_door["height"])
+                                    add_tree_node("Width", current_door["width"])
+                                    add_tree_node("Side", current_door["side"])
 
 
 def add_tree_node(parameter_name, value):
